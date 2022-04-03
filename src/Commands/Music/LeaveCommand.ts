c63b07919425f7a097a5aba624a51912a6d85bc3
Kayxue <s0990006@gmail.com>
2022-04-03 23:23:06 +0800

    2022/04/03 finished Stop, Leave, Resume, NowPlaying Command

---
+++ src/Commands/Music/LeaveCommand.ts
@@ -0,0 +1,29 @@
+import { Client, Message } from "discord.js";
+import ICommand from "../../Interfaces/ICommand";
+import ICommandInfo from "../../Interfaces/ICommandInfo";
+
+export default class Pause implements ICommand {
+    private client: Client;
+
+    public constructor(client: Client) {
+        this.client = client;
+    }
+    public getInfo(): ICommandInfo {
+        return {
+            name: "leave",
+        };
+    }
+    public async handle(message: Message, args: string[]): Promise<any> {
+        const voiceChannel = message.member.voice.channel;
+        if (!voiceChannel) return message.channel.send("請加入一個語音頻道");
+        const myVoiceChannel = message.guild.me.voice.channel;
+        if (!myVoiceChannel)
+            return message.channel.send("我不在一個語音頻道！");
+        if (voiceChannel.id !== myVoiceChannel.id)
+            return message.channel.send("你跟我在不同語音頻道！");
+
+        const player = this.client.distube.queues.get(message)
+        if (player && !player.stopped) await player.stop()
+        return this.client.distube.voices.get(message).leave()
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import Client from "./Client"
 import { token, prefix } from "./Config"
-const client=new Client({token,prefix})
+const client = new Client({ token, prefix })
 client.start()
